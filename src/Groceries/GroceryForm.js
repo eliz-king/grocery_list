4a92f55d30120967d4dd69619b67833abfba25df
Elizabeth King <elizking.dev@gmail.com>
2019-03-21 19:04:04 -0600

    added form and list

--- src/Groceries/GroceryForm.js
+++ src/Groceries/GroceryForm.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class GroceryForm extends Component {
+  state = { name: '' }
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addItem(this.state.name)
+    this.setState({name: ''})
+  }
 
+  handleChange = (e) => {
+    this.setState({ name: e.target.value })
+  }
+ 
+  render() {
+    const { name } = this.state
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          required
+          placeholder="add an item"
+          value={name}
+          name="Items"
+          onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
+}
 
 export default GroceryForm;
